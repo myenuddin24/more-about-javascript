b10f2157156f9d77366c3b303b62b3a417072d44
Myen Uddin <mohiuddinbhuiyan030@gmail.com>
2026-09-19 14:02:06 +0600

    about-js file created

---
+++ about-js.js
@@ -0,0 +1,41 @@
+/*
+JavaScript is a:
+
+1. JIT (Just-In-Time) Compiled Language
+   - JavaScript code is compiled and executed at runtime.
+   - It helps JavaScript run efficiently.
+
+2. Multi-Paradigm Language
+   - JavaScript supports different programming styles.
+   - Example: Procedural, Object-Oriented, Functional programming.
+
+3. Prototype-Based Language
+   - JavaScript uses prototypes for inheritance.
+   - Objects can inherit properties and methods from other objects
+     through the prototype chain.
+
+4. Dynamically Typed Language
+   - We don't need to declare a variable's data type.
+   - The type can change during runtime.
+
+   Example:
+   let value = 10;
+   value = "Hello";
+
+5. Garbage Collection
+   - JavaScript automatically removes objects/data that are no longer
+     needed from memory.
+   - This helps manage memory automatically.
+
+6. High Abstraction:
+
+- Hides complex, low-level implementation details.
+- Allows developers to focus on what the program should do,
+  rather than how the computer does it.
+
+Example:
+console.log("Hello");
+
+We don't need to know how the browser internally handles
+memory, system calls, or output operations.
+*/
